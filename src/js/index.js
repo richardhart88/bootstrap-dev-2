fa0be41e8c741c9d8b17b0046030e31553e8283e
Richard Hart <tuba1388@yahoo.com>
2022-03-03 12:14:33 -0600

    added button to toggle theme

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,36 @@
-const darkTheme = "https://bootswatch.com/4/cyborg/bootstrap.min.css"
-const darkLink = document.getElementById("dark-theme")
-const toggleTheme = document.getElementById("theme-toggle")
-
-function toggleColor() {
-  enableDarkTheme()
+// you can use app's unique identifier here
+const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
+const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+// you can change this url as needed
+const darkTheme = "https://bootswatch.com/4/cyborg/bootstrap.min.css";
+const darkLink = document.getElementById("dark-theme-style");
+const themeToggler = document.getElementById("theme-toggler");
+let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
+// check if user has already selected dark theme earlier
+if (isDark) {
+  enableDarkTheme();
+} else {
+  disableDarkTheme();
+}
+/**
+ * Apart from toggling themes, this will also store user's theme preference in local storage.
+ * So when user visits next time, we can load the same theme.
+ *
+ */
+ function toggleColor() {
+  if (darkLink.getAttribute('href') === darkTheme) {
+    disableDarkTheme()
+  } else {
+    enableDarkTheme()
+  }
 }
 
 function enableDarkTheme() {
-  darkLink.setAttribute('href', darkTheme)
+  darkLink.setAttribute("href", darkTheme);
+  themeToggler.innerHTML = "🌙 Dark";
 }
 
 function disableDarkTheme() {
-  darkLink.setAttribute('href', '')
+  darkLink.setAttribute("href", "");
+  themeToggler.innerHTML = "🌞 Light";
 }
